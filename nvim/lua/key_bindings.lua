d07d9d4a6ff64734124b5a108392dcd5dadea823
Sam <dev@samarthj.com>
2022-11-17 17:03:47 -0800

    fix(nvim): resolve treesitter errors

--- nvim/lua/key_bindings.lua
+++ nvim/lua/key_bindings.lua
@@ -4,11 +4,19 @@ local function map(mode, lhs, rhs, opts)
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
+-- Find file in current dir
 map('n', '<C-p>', '<cmd>lua require("telescope.builtin").find_files()<cr>')
-map('n', '<C-q>', '<cmd>lua require("telescope.builtin").file_browser()<cr>')
-map('n', '<C-b>', '<cmd>lua require("telescope.builtin").buffers()<cr>')
+-- Only works in the previous scope, .... forgot why this was useful, removing for now
+-- -- -- map('n', '<C-q>', '<cmd>lua require("telescope.builtin").file_browser()<cr>')
+-- Find text in file in current dir
 map('n', '<C-s>', '<cmd>lua require("telescope.builtin").live_grep()<cr>')
+-- Switch between files in the current session
+map('n', '<C-b>', '<cmd>lua require("telescope.builtin").buffers()<cr>')
+-- Search current file
 map('n', '<C-f>', '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find()<cr>')
+-- Cycle through treesitter AST collected references
 map('n', '<C-o>', '<cmd>lua require("telescope.builtin").treesitter()<cr>')
+-- Cycle through lsp references of current selection
 map('n', '<C-j>', '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
-map('n', '<C-a>', '<cmd>lua require("telescope.builtin").builtins()<cr>')
+-- Broken? (or forgot what this was for)
+-- map('n', '<C-a>', '<cmd>lua require("telescope.builtin").builtins()<cr>')
